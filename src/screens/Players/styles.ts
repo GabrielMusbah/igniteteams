b61e4bc9e9a493cbdfdb97e7378384053cc7b9a6
MusbahPlantae <gabrielmusbah19@gmail.com>
2023-05-30 07:32:10 -0400

    test-fork

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,6 +1,6 @@
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import styled, { css } from "styled-components/native";
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
